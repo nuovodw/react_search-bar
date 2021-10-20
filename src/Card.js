94b8893b19d4ae0adb8a62365c65db4988a5b3cb
Daniel Won <dan.j.won@gmail.com>
2021-10-19 21:25:12 -0500

    create Card component and add Tachyon

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+const Card = () => {
+	return (
+		<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+			<img src="https://robohash.org/test?200x200" alt="robot" />
+			<div>
+				<h2>Jane Doe</h2>
+				<p>jane.doe@gmail.com</p>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
